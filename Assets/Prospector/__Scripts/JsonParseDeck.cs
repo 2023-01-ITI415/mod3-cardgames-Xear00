e1e6459bf48776f3d7541ee88bba2a3d954d377b
Xear <zzier42@gmail.com>
2023-03-29 16:31:33 -0400

    added code for deck assembly

--- Assets/Prospector/__Scripts/JsonParseDeck.cs
+++ Assets/Prospector/__Scripts/JsonParseDeck.cs
@@ -26,6 +26,8 @@ public class JsonDeck{
 
 public class JsonParseDeck : MonoBehaviour
 {
+    private static JsonParseDeck S {get; set; }
+
     [Header("Inscribed")]
     public TextAsset jsonDeckFile;
 
@@ -33,7 +35,26 @@ public class JsonParseDeck : MonoBehaviour
     public JsonDeck deck;
 
     void Awake(){
+        if (S != null){
+            Debug.LogError("JsonParseDeck.S can't be set a 2nd time!");
+            return;
+        }
+        S = this;
+
         deck = JsonUtility.FromJson<JsonDeck>(jsonDeckFile.text);
     }
     
+
+    static public List<JsonPip> DECORATORS{
+        get {return S.deck.decorators;}
+    }
+
+    static public JsonCard GET_CARD_DEF (int rank) {
+        if ((rank < 1) || (rank > S.deck.cards.Count)) {
+            Debug.LogWarning("Illegal rank argument: " +rank);
+            return null;
+        }
+        return S.deck.cards[rank - 1];
+    }
+
 }
