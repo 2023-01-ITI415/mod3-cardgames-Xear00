84b9d3a994ce8223eb63756ad2880cf2607b2a95
Xear <zzier42@gmail.com>
2023-03-29 17:34:59 -0400

    Numbers added to cards

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -28,9 +28,56 @@ public class Card : MonoBehaviour
         }
 
         def = JsonParseDeck.GET_CARD_DEF(rank);
+
+        AddDecorators();
     }
 
     public virtual void SetLocalPos(Vector3 v){
         transform.localPosition = v;
     }
+
+    //private variables that will be reused
+    private Sprite _tSprite = null;
+    private GameObject _tGO = null;
+    private SpriteRenderer _tSRend = null;
+
+    //An Euler rotation of 180 degrees around the Z-axis will flip the sprites upside down
+    private Quaternion _flipRot = Quaternion.Euler(0, 0, 180);
+
+
+    private void AddDecorators(){
+        foreach (JsonPip pip in JsonParseDeck.DECORATORS){
+            if (pip.type == "suit"){
+                _tGO = Instantiate<GameObject>(Deck.SPRITE_PREFAB,transform);
+                //get the SpriteRenderer component
+                _tSRend = _tGO.GetComponent<SpriteRenderer>();
+                //get the suit Sprite from the CardSpritesSO.SUIT static field
+                _tSRend.sprite = CardSpritesSO.SUITS[suit];
+            } else {
+                _tGO = Instantiate<GameObject>(Deck.SPRITE_PREFAB, transform);
+                _tSRend = _tGO.GetComponent<SpriteRenderer>();
+                _tSRend.sprite = CardSpritesSO.RANKS[rank];
+                _tSRend.color = color;
+            }
+
+        _tSRend.sortingOrder = 1;
+        _tGO.transform.localPosition = pip.loc;
+        //flip the decorator if needed
+        if (pip.flip) _tGO.transform.rotation = _flipRot;
+        //set the scale to keep decorators from being too bug
+        if (pip.scale != 1 ){
+            _tGO.transform.localScale = Vector3.one * pip.scale;
+
+        }
+
+        _tGO.name = pip.type;
+
+        decoGOs.Add(_tGO);
+
+        }
+    }
+
+
+
+
 }
