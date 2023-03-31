9557ff2f05d22a72101e60fed1d6659ff8eb38bd
Xear00 <zzier42@gmail.com>
2023-03-30 20:32:46 -0400

    Added mine function of Prospector.cs

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -12,7 +12,11 @@ public class Prospector : MonoBehaviour
 
     [Header("Dynamic")]
     public List<CardProspector> drawPile;
+    public List<CardProspector> discardPile;
+    public List<CardProspector> mine;
+    public CardProspector target;
 
+    private Transform layoutAnchor;
     private Deck deck;
     private JsonLayout jsonLayout;
 
@@ -29,6 +33,8 @@ public class Prospector : MonoBehaviour
         deck.InitDeck();
         Deck.Shuffle(ref deck.cards);
         drawPile = ConvertCardsToCardProspectors(deck.cards);
+
+        LayoutMine();
     }
 
     List<CardProspector> ConvertCardsToCardProspectors(List<Card> listCard)
@@ -42,6 +48,46 @@ public class Prospector : MonoBehaviour
         return (listCP);
     }
 
-    // Update is called once per frame
+    CardProspector Draw()
+    {
+        CardProspector cp = drawPile[0];
+        drawPile.RemoveAt(0);
+        return (cp);
+    }
+
+
+    void LayoutMine()
+    {
+        if (layoutAnchor == null)
+        {
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+        }
+
+        CardProspector cp;
+
+        foreach (JsonLayoutSlot slot in jsonLayout.slots)
+        {
+            cp = Draw();
+            cp.faceUp = slot.faceUp;
+            //make the cardprospector a child of layoutAnchor
+            cp.transform.SetParent(layoutAnchor);
+            //conver last char of layer string to an int
+            int z = int.Parse(slot.layer[slot.layer.Length - 1].ToString());
+
+            cp.SetLocalPos(new Vector3(
+                jsonLayout.multiplier.x * slot.x,
+                jsonLayout.multiplier.x * slot.y,
+                -z));
+
+            cp.layoutID = slot.id;
+            cp.layoutSlot = slot;
+
+            cp.state = eCardState.mine;
+
+            mine.Add(cp);
+        }
+    }
+
 
 }
